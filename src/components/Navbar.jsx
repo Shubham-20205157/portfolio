ebe2e12fa0df4f52fc4f3c3d092c52807bee6d60
Shubahm <shubham.20205157@mnnit.ac.in>
2024-02-05 16:23:39 +0530

    Adding models

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,14 @@ import { NavLink } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <header className='header'>
+        
         <NavLink to = '/' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
-            <p className='blue-gradient_text'>SK</p>
+            <div className="group relative">
+                <div className="blue-gradient_text">SK</div>
+                <div className="hidden group-hover:block absolute bg-gray-200 p-4 mt-2">
+                    <p>Shubham Kushwaha</p>
+                </div>
+            </div>
         </NavLink>
         <nav className='flex text-lg gap-7 font-medium'>
             <NavLink to = '/about' className={({isActive})=> isActive?'text-blue-500': 'text-black'}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Loader } from '../components/Loader'
 export const Home = () => {
   return (
-    <section className='w-full h-screen relative'></section>
+    <section className='w-full h-screen relative'>
+        <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1 , far: 1000}}>
+            <Suspense fallback = {<Loader />}/>
+        </Canvas>
+    </section>
   )
 }
