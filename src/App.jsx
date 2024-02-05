4405622638a51fc9d4b3e143a418cfde5513fbff
Shubahm <shubham.20205157@mnnit.ac.in>
2024-02-05 15:03:50 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import {Route , BrowserRouter as Router ,Routes} from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { About } from './pages/About';
+import {Home} from './pages/Home';
+import { Projects } from './pages/Projects';
 export const App = () => {
   return (
-    <div>App</div>
+    <main className='bg-slate-100/20'>
+        <Router>
+            < Navbar />
+            <Routes>
+                <Route path = "/" element = {<Home />} />
+                <Route path = "/about" element = {<About />} />
+                <Route path = "/project" element = {<Projects />} />
+            </Routes>
+        </Router>
+        
+    </main>
+   
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <section className='w-full h-screen relative'></section>
+  )
+}
